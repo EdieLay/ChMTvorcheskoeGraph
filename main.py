dd2f67af3a31e8f70521375f8dc0871e0c96f8a6
EdieLay <arkhip_as_boss@list.ru>
2023-12-20 10:49:43 +0300

    Init

---
+++ main.py
@@ -0,0 +1,24 @@
+import plotly.graph_objs as go
+import pandas as pd
+
+def main():
+    with open('C:/Users/user/source/repos/ChMTvorcheskoe1/ChMTvorcheskoe1/points.txt', 'r') as f:
+        n = int(f.readline())
+        print(n)
+        x = f.readline().split()
+        x = list(map(lambda el: float(el), x))
+        print(x)
+        fx = f.readline().split()
+        fx = list(map(lambda el: float(el), fx))
+        print(fx)
+        px = f.readline().split()
+        px = list(map(lambda el: float(el), px))
+        print(px)
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x=x, y=fx, mode='lines+markers', name='f(x)'))
+        fig.add_trace(go.Scatter(x=x, y=px, mode='markers', name='P(x)'))
+        fig.show()
+
+if __name__ == '__main__':
+    main()
+
